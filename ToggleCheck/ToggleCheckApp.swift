5fd44f94179a74a795123b97b7f4b22e0500bb0a
nke <nke@nke.ru>
2023-03-22 22:01:14 +0700

    Initial Commit

---
+++ ToggleCheck/ToggleCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleCheckApp.swift
+//  ToggleCheck
+//
+//  Created by admin on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToggleCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
