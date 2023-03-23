6a9f2f50d78d873e7d1d834f37cdc88f44834a14
nke <nke@nke.ru>
2023-03-23 07:28:23 +0700

    sheet half

--- ToggleCheck/ContentView.swift
+++ ToggleCheck/ContentView.swift
@@ -7,28 +7,80 @@
 
 import SwiftUI
 
-
-
+//import BottomSheet
+//import ResizableSheeet
     
 
 struct ContentView: View {
-    @State var isAlert = true
+    @State var isAlert = false
     @State private var vibrateOnRing = false
+    @State var isSheet = false
+//    @State private var bottomSheetPosition: BottomSheetPosition = .middle
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("sheet"){
+                isSheet = true
+            }
             Toggle(isOn: $vibrateOnRing) {
                 Text("Vibrate on Ring")
             }
         }.alert(isPresented: $isAlert){
             Alert(title: Text("Ошибка"),dismissButton: .default(Text("OK")))
         }
+       
         .padding()
+        .sheet(isPresented: $isSheet){
+            Text("Content")
+               // .presentationDetents([.medium, .large])
+                    .presentationDetents([.height(200), .medium, .large])
+//                    .presentationDragIndicator(.automatic)
+        }
+//        .bottomSheet(bottomSheetPosition: self.$bottomSheetPosition, content: {
+//                            //The Numbers from 0 to 99 as Main Content in a Scroll View
+//                            ScrollView {
+//                                ForEach(0..<100) { index in
+//                                    Text(String(index))
+//                                }
+//                                .frame(maxWidth: .infinity)
+//                            }
+//                            .padding(.top)
+//                        })
     }
+   
 }
+
+//extension View{
+//    func halfSheet<SheetView: View>(showSheet: Binding<Bool>,
+//      @ViewBuilder SheetView: @escaping()->SheetView)-> some View{
+//
+//        return self
+//            .background(
+//             HalfSheetHelper(sheetView: SheetView(), isSheet: showSheet)
+//            )
+//    }
+//}
+//
+//
+//
+//struct HalfSheetHelper<SheetView: View>: UIViewControllerRepresentable{
+//    var sheetView: SheetView
+//    @Binding var isSheet: Bool
+//    let controller =  UIViewController()
+//
+//    func makeUIViewController(context: Context) -> UIViewController {
+//        controller.view.backgroundColor = .clear
+//        return controller
+//    }
+//
+//    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+//
+//    }
+//}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
